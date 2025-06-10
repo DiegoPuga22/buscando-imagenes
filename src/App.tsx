8d47895a91b8e77728c7d5c9e53d3a8e68044544
DiegoPuga22 <2023171060@uteq.edu.mx>
2025-06-09 21:40:23 -0600

    Probando con botón2

--- src/App.tsx
+++ src/App.tsx
@@ -65,7 +65,7 @@ function App() {
           placeholder="Busca imágenes..."
           className="search-input"
         />
-        <button type="submit" className="search-button">Buscar final</button>
+        <button type="submit" className="search-button">Buscar final 2</button>
       </form>
 
       {loading && <p>Cargando imágenes...</p>}
